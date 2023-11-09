beba010a5c2806a484a429d3cf9d8de1e1ad525e
elen-oz <ozerova.lenka@gmail.com>
2023-11-09 19:27:34 +0100

    feat: create page & styling

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const rulesBtn = document.querySelector('#rules-btn');
+const closeBtn = document.querySelector('#close-btn');
+const rulesEl = document.querySelector('#rules');
+
+rulesBtn.addEventListener('click', () => {
+  rulesEl.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  rulesEl.classList.remove('show');
+});
