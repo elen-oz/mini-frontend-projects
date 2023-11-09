853cb9a01c24fdf374ea42689995b16864944793
elen-oz <ozerova.lenka@gmail.com>
2023-11-09 21:47:39 +0100

    feta: add bricks

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -8,6 +8,9 @@ const ctx = canvasEl.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 const ball = {
   x: canvasEl.width / 2,
   y: canvasEl.height / 2,
@@ -26,6 +29,25 @@ const paddle = {
   dx: 0,
 };
 
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i += 1) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j += 1) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
 const drawBall = () => {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
@@ -42,16 +64,29 @@ const drawPaddle = () => {
   ctx.closePath();
 };
 
-const draw = () => {
-  drawBall();
-  drawPaddle();
-  drawScore();
-};
-
 const drawScore = () => {
   // ctx.font= '1.5rem Arial';
   ctx.font = '1.5rem Courier';
   ctx.fillText(`Score: ${score}`, canvasEl.width - 100, 30);
+};
+
+const drawBricks = () => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? 'cornflowerblue' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+};
+
+const draw = () => {
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
 };
 
 draw();
